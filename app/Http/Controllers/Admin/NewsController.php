9bed51a193a794239478d464e1d77eab1623cb9d
onozawaryo <f91winter121@ezweb.ne.jp>
2019-11-25 04:44:54 +0000

    first commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+}
---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+        <h2>今日の格言&ことわざ</h2>
+        <p>塞翁が馬</p>
+        <p>岡目八目</p>
+        <p>飛んで火にいる夏の虫</p>
+        <p>豚に真珠</p>
+        <p>墓穴を掘る</p>
+    </body>
+</html>
